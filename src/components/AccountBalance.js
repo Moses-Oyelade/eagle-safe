7e372333f1412be2b7e27a0f2861c53469710d10
Moses-Oyelade <oyelademoses@gmail.com>
2023-09-21 19:55:48 +0100

    try account balance function code

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -1,34 +1,41 @@
 // import React from 'react'
 
-// function AccountBalance({ onUpdatedItem}) {
+// function AccountBalance({ setTransactions, transactions}) {
 
-// // const [Balance, setBalance] = useState()
-
-
+// // const [Balance, setBalance] = useState(" ")
 
 
-//   function handleNewBalance(e){ 
-//     e.preventDefault();
 
 
-//     fetch(`http://localhost:3000/transactions/${transactions.id}`,{
-//       method: "PATCH",
-//       headers:{
-//         "Content-Type" : "application/json",
-//       },
-//       body : JSON.stringify({transactions: {amount: amount}})
-//     })
-//     .then(res => console.log(res))
-//     .then(updatedItem =>  onUpdatedItem(updatedItem))
+// //   function handleNewBalanceClick(e){ 
+// //     e.preventDefault();
+// //      console.log("clicked Acc Bal:", transaction);
 
-//     // setBalance()
+
+// //     fetch(`http://localhost:3000/transactions/${transactions.id}`,{
+// //       method: "PATCH",
+// //       headers:{
+// //         "Content-Type" : "application/json",
+// //       },
+// //       body : JSON.stringify({transactions: {amount: amount}})
+// //     })
+// //     .then(res => console.log(res))
+// //     .then(updatedItem =>  onUpdatedItem(updatedItem))
+
+// //     // setBalance()
+
+// function handleUpdatedTransactions(updatedBalance){
+//     console.log("in balance:", updatedBalance);
+//     const upDatedItems = transactions.map(item => item.amount).reduce((prev, next) => prev + next);
+//      console.log(upDatedItems);
+//     setTransactions(upDatedItems)
+//     return upDatedItems;
+//   };
 
     
+// // }
 
-    
-// }
-
-// // console.log(transactions.map(amount).reduce(sum));
+// // // console.log(transactions.map(amount).reduce(sum));
  
 
 
@@ -40,10 +47,10 @@
 //       <h4>SAVINGS ACCOUNT</h4>
 //       <h4>0027856679</h4>
 //       <button id="balance" 
-//         onClick={handleNewBalance}
+//         onClick={handleUpdatedTransactions}
 //       >Account Balance
 //       </button>
-//       <p>{}</p>
+//       <p>{updatedBalance}</p>
       
 //       </div>
 //   )
